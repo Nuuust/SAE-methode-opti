ccca19967ab54974e6be6a54c04d28e2fceb1603
nathanmariedev <nathan.marie030303@gmail.com>
2023-12-04 17:48:42 +0100

    Co-authored-by: Nuuust <Nuuust@users.noreply.github.com>

--- data/data-details.md
+++ data/data-details.md
@@ -3,16 +3,19 @@
 
 ## reviews
 
-- overall *float*
-- vote *int*
+- image - images that users post after they have received the product *list[string]*
+- overall - rating of the product *float*
+- vote - helpful votes of the review *int*
 - verified *bool*
-- reviewTime *string* (mm d, yyyy)
-- reviewerID *string*
-- asin *string* 
-- reviewerName *string*
-- reviewText *string*
-- summary *string*
-- unixReviewTime *int*
+- reviewTime - time of the review (raw) *string* (mm d, yyyy)
+- reviewerID - ID of the reviewer *string*
+- asin - ID of the product *string* 
+- style - a disctionary of the product metadata *dict*
+- reviewerName - name of the reviewer *string*
+- reviewText - text of the review *string*
+- summary - summary of the review *string*
+- unixReviewTime - time of the review (unix time) *int*
+
 
 
 ## meta
@@ -30,3 +33,8 @@
 - asin *string*
 - imageURL *list*
 - imageURLHighRes *list*
+
+
+
+doublon = meme reviewer meme produit
+apprentissage 100 000 par année = voir évolution des avis 
--- main.py
+++ main.py
@@ -11,6 +11,49 @@ YELLOW = '\033[93m' # INFORMATIONS
 
 print(BOLD+BLUE+"\n\nChargement des données...\n\n*")
 df = pd.concat([chunk for chunk in tqdm(pd.read_json('./data/Video_Games.json', lines=True, chunksize=1000), desc='Loading data')])
+df = df.drop(['style', 'image'], axis=1)
 
 print(BLUE+'*\n\n'+RESET+BOLD+"Dimensions du DataFrame => "+YELLOW+f"{df.shape}" + RESET)
+print(BOLD+"\nColonnes => "+YELLOW+f"{df.columns.to_list()}"+RESET)
 print(BOLD+"\nNombre de textes d'avis null => "+YELLOW+f"{df['reviewText'].isnull().sum()}"+RESET)
+print(BOLD+"\nNombre de titres d'avis null => "+YELLOW+f"{df['summary'].isnull().sum()}"+RESET)
+#print(BOLD+"\nNombre de notes null => "+YELLOW+f"{df['overall'].isnull().sum()}"+RESET)
+print(BOLD+"\nNombre de votes null => "+YELLOW+f"{df['vote'].isnull().sum()}"+RESET)
+print(df.head()['vote'])
+
+# TRI PAR ANNÉES
+
+dfs_by_year = {}
+
+# Iterate through the rows of the original DataFrame
+for index, row in df.iterrows():
+    year = row['reviewTime'].split(" ")[2]
+    
+    # Check if a DataFrame for the year exists, create one if not
+    if year not in dfs_by_year:
+        print(f'Creating DataFrame for year {year}...')
+        dfs_by_year[year] = pd.DataFrame(columns=df.columns)
+        df_y=pd.DataFrame(row)
+        pd.concat([df_y, dfs_by_year[year]], ignore_index=True)
+    
+    # Check if vote exists
+    if pd.isnull(row["vote"]):
+        df.iloc[index, df.columns.get_loc("vote")] = 0.0
+    
+    
+
+    #dfs_by_year[year].append(row, ignore_index=True)
+    #dfs_by_year[year].insert(len(dfs_by_year[year].columns), index, row) ------->  NOT WORKING ON LARGE DATAFRAME
+
+
+
+pd.concat(dfs_by_year[year], ignore_index=True)
+print(dfs_by_year)
+    # print(dfs_by_year[year].head())    
+    #dfs_by_year[year] = pd.concat([dfs_by_year[year], pd.DataFrame([row])], ignore_index=True)
+    #dfs_by_year[year].loc[len(dfs_by_year[year])] = row
+print(BOLD+"\nNombre de votes null => "+YELLOW+f"{df['vote'].isnull().sum()}"+RESET)
+
+# for year, df_year in dfs_by_year.items():
+#     print(f'DataFrame for year {year}:')
+#     print(df_year)
