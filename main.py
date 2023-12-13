864ccdf6df07d3f00119eafe8a0376f0ea015c2b
Benjamin COUET <nust660@gmail.com>
2023-12-13 17:48:45 +0100

    data analyze

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 import pandas as pd
 from tqdm import tqdm
 import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+import spacy
 
 BOLD = '\033[1m' # ACTIONS
-BLUE = '\033[94m' # ACTIONS
+BLUE = '\033[94m' # ACTIONSv
 RESET = '\033[0m'
 RED = '\033[91m' # ERRORS
 GREEN = '\033[92m' # SUCCESS
@@ -17,16 +23,12 @@ df = df.drop(['style', 'image'], axis=1)
 
 df['vote'] = df['vote'].fillna(0.0)
 
-# Adapting to 'reviewTime' column with the format 'mm d, yyyy'
 df['reviewTime'] = pd.to_datetime(df['reviewTime'], format='%m %d, %Y')
 
-# Extract year from 'reviewTime'
 df['year'] = df['reviewTime'].dt.year
 
-# Creating a directory to store the split DataFrames
 os.makedirs('./split_data', exist_ok=True)
 
-# Splitting the DataFrame by year and saving each as a separate file
 for year, group in df.groupby('year'):
     if not os.path.exists(f'./split_data/reviews_{year}.json'):
         group.to_json(f'./split_data/reviews_{year}.json', orient='records', lines=True)
@@ -47,3 +49,53 @@ print(BOLD+"\nNombre de textes d'avis null => "+YELLOW+f"{df['reviewText'].isnul
 print(BOLD+"\nNombre de titres d'avis null => "+YELLOW+f"{df['summary'].isnull().sum()}"+RESET)
 print(BOLD+"\nNombre de notes null => "+YELLOW+f"{df['overall'].isnull().sum()}"+RESET)
 print(BOLD+"\nNombre de votes null => "+YELLOW+f"{df['vote'].isnull().sum()}"+RESET)
+
+
+'''
+df=pd.read_json('./split_data/reviews_2014.json', lines=True)
+
+df['label'] = df['overall'].apply(lambda x: 1 if x > 3 else 0) 
+
+# Prétraitement avec spaCy
+nlp = spacy.load("en_core_web_sm")
+def preprocess(text):
+    doc = nlp(text)
+    return ' '.join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
+
+df['processed_text'] = df['reviewText'].apply(preprocess)
+
+
+
+
+
+
+# Vectorisation
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df['processed_text'])
+y = df['label']
+
+# Division des données
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# KNN
+knn_model = KNeighborsClassifier()
+knn_params = {'n_neighbors': [3, 5, 7, 9]}
+knn_clf = GridSearchCV(knn_model, knn_params)
+knn_clf.fit(X_train, y_train)
+
+# Régression logistique
+log_model = LogisticRegression()
+log_params = {'C': [0.01, 0.1, 1, 10]}
+log_clf = GridSearchCV(log_model, log_params)
+log_clf.fit(X_train, y_train)
+
+# Évaluation de KNN
+y_pred_knn = knn_clf.predict(X_test)
+print("KNN Classification Report")
+print(classification_report(y_test, y_pred_knn))
+
+# Évaluation de la Régression logistique
+y_pred_log = log_clf.predict(X_test)
+print("Logistic Regression Classification Report")
+print(classification_report(y_test, y_pred_log))
+'''
