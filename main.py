f6bc31edd09002d713991d4b0f893530ddb10737
nathanmariedev <nathan.marie030303@gmail.com>
2023-12-17 16:45:50 +0100

    work

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import spacy
+from tqdm import tqdm
 
 BOLD = '\033[1m' # ACTIONS
 BLUE = '\033[94m' # ACTIONSv
@@ -16,57 +17,74 @@ RED = '\033[91m' # ERRORS
 GREEN = '\033[92m' # SUCCESS
 YELLOW = '\033[93m' # INFORMATIONS
 
-# print(BOLD+BLUE+"\n\nChargement des données...\n\n*")
-df = pd.concat([chunk for chunk in tqdm(pd.read_json('./data/Video_Games.json', lines=True, chunksize=1000), desc=BLUE+'Chargement des données')])
-df = df.drop(['style', 'image'], axis=1)
+def traitementData():
+    print(BOLD+BLUE+"\n\nChargement des données...\n\n*")
+    df = pd.concat([chunk for chunk in tqdm(pd.read_json('./data/Video_Games.json', lines=True, chunksize=1000), desc=BLUE+'Chargement des données')])
+    df = df.drop(['style', 'image'], axis=1)
 
 
-df['vote'] = df['vote'].fillna(0.0)
+    df['vote'] = df['vote'].fillna(0.0)
 
-df['reviewTime'] = pd.to_datetime(df['reviewTime'], format='%m %d, %Y')
+    df['reviewTime'] = pd.to_datetime(df['reviewTime'], format='%m %d, %Y')
 
-df['year'] = df['reviewTime'].dt.year
+    df['year'] = df['reviewTime'].dt.year
 
-os.makedirs('./split_data', exist_ok=True)
+    os.makedirs('./split_data', exist_ok=True)
 
-for year, group in df.groupby('year'):
-    if not os.path.exists(f'./split_data/reviews_{year}.json'):
-        group.to_json(f'./split_data/reviews_{year}.json', orient='records', lines=True)
-        print(GREEN + f'Fichier JSON créé pour l\'année {year}' + RESET)
-    else:
-        print(GREEN + f'Fichier JSON pour l\'année {year} existe déjà !' + RESET)
-    
-    group=group.drop_duplicates(subset=["asin","reviewerID","vote"], keep='last', inplace=True)
-    if group!=None:
-        print(f"{RED}Problème de doublons sur : {group}")
-    else:
-        print(f"{YELLOW}PAS DE DOUBLONS")
-
-
-print(BLUE+'*\n\n'+RESET+BOLD+"Dimensions du DataFrame => "+YELLOW+f"{df.shape}" + RESET)
-print(BOLD+"\nColonnes => "+YELLOW+f"{df.columns.to_list()}"+RESET)
-print(BOLD+"\nNombre de textes d'avis null => "+YELLOW+f"{df['reviewText'].isnull().sum()}"+RESET)
-print(BOLD+"\nNombre de titres d'avis null => "+YELLOW+f"{df['summary'].isnull().sum()}"+RESET)
-print(BOLD+"\nNombre de notes null => "+YELLOW+f"{df['overall'].isnull().sum()}"+RESET)
-print(BOLD+"\nNombre de votes null => "+YELLOW+f"{df['vote'].isnull().sum()}"+RESET)
+    for year, group in df.groupby('year'):
+        if not os.path.exists(f'./split_data/reviews_{year}.json'):
+            group.to_json(f'./split_data/reviews_{year}.json', orient='records', lines=True)
+            print(GREEN + f'Fichier JSON créé pour l\'année {year}' + RESET)
+        else:
+            print(GREEN + f'Fichier JSON pour l\'année {year} existe déjà !' + RESET)
+        
+        group=group.drop_duplicates(subset=["asin","reviewerID","vote"], keep='last', inplace=True)
+        if group!=None:
+            print(f"{RED}Problème de doublons sur : {group}")
+        else:
+            print(f"{YELLOW}PAS DE DOUBLONS")
 
 
-'''
-df=pd.read_json('./split_data/reviews_2014.json', lines=True)
+    print(BLUE+'*\n\n'+RESET+BOLD+"Dimensions du DataFrame => "+YELLOW+f"{df.shape}" + RESET)
+    print(BOLD+"\nColonnes => "+YELLOW+f"{df.columns.to_list()}"+RESET)
+    print(BOLD+"\nNombre de textes d'avis null => "+YELLOW+f"{df['reviewText'].isnull().sum()}"+RESET)
+    print(BOLD+"\nNombre de titres d'avis null => "+YELLOW+f"{df['summary'].isnull().sum()}"+RESET)
+    print(BOLD+"\nNombre de notes null => "+YELLOW+f"{df['overall'].isnull().sum()}"+RESET)
+    print(BOLD+"\nNombre de votes null => "+YELLOW+f"{df['vote'].isnull().sum()}"+RESET)
+
+
+
+#traitementData()
+
+df=pd.read_json('./split_data/reviews_2000.json', lines=True)
+df = df.dropna(subset=['reviewText'])
+df = df[df['reviewText'] != '']
 
 df['label'] = df['overall'].apply(lambda x: 1 if x > 3 else 0) 
+if not df['reviewText'].notna().all():
+    print("Certains reviewText sont null")
+else:
+    print("Aucun reviewText null")
 
-# Prétraitement avec spaCy
+
 nlp = spacy.load("en_core_web_sm")
+t = tqdm(total=df.shape[0], desc="Prétraitement des textes")
 def preprocess(text):
     doc = nlp(text)
+    t.update(1)
     return ' '.join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
 
-df['processed_text'] = df['reviewText'].apply(preprocess)
+# Prétraitement avec spaCy
+def traitementLinguistique(): 
+    print(BOLD+BLUE+"\n\nTraitement linguistique...\n\n*")
+    try:
+        df['processed_text'] = df['reviewText'].apply(preprocess)
+        print(GREEN+"\nTraitement liguistique réussi"+RESET)
+    except:
+        print(RED+"\nErreur lors du traitement linguistique"+RESET)
+        
 
-
-
-
+traitementLinguistique()
 
 
 # Vectorisation
@@ -83,6 +101,7 @@ knn_params = {'n_neighbors': [3, 5, 7, 9]}
 knn_clf = GridSearchCV(knn_model, knn_params)
 knn_clf.fit(X_train, y_train)
 
+'''
 # Régression logistique
 log_model = LogisticRegression()
 log_params = {'C': [0.01, 0.1, 1, 10]}
